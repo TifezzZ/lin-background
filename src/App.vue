9fb40a6a43224b4e76713ba56fffad9ec9b2992d
yujingbo <TifezzZ@DESKTOP-7N2EP1T>
2023-03-13 14:47:16 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<!--
+ * @Author: yujingbo
+ * @Date: 2023-03
+ * @LastEditors: yujingbo
+ * @LastEditTime: 2023-03
+ * @Description: 
+-->
+<script setup lang="ts"></script>
 
 <template>
   <div>
@@ -11,7 +16,6 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
