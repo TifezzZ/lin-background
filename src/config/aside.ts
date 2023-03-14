b019eb6c043306d8436d38fc896ab073432b002c
yujingbo <TifezzZ@DESKTOP-7N2EP1T>
2023-03-14 16:50:39 +0800

    feat: aside develop

--- src/config/aside.ts
+++ src/config/aside.ts
@@ -7,64 +7,57 @@
  */
 export default [
   {
+    path: '/myAccount/index',
+    name: 'myAccount',
+    component: '/myAccount/index',
     meta: {
-      icon: 'application',
-      title: '资源中心'
-    },
-    children: [
-      {
-        path: '/dataset/index',
-        name: 'dataset',
-        component: '/dataset/index',
-        meta: {
-          icon: 'dataset',
-          title: '数据管理'
-        }
-      }
-    ]
-  },
-  {
-    path: '/model/index',
-    name: 'model',
-    component: '/model/index',
-    meta: {
-      icon: 'model',
-      title: '模型仓库'
+      icon: '',
+      title: '我的账户'
     }
   },
   {
-    path: '/assess/index',
-    name: 'assess',
-    component: '/assess/index',
+    path: '/mySold/index',
+    name: 'mySold',
+    component: '/mySold/index',
     meta: {
-      icon: 'assess',
-      title: '测评管理'
+      icon: '',
+      title: '我卖出的'
     }
   },
   {
+    path: '/myPublished/index',
+    name: 'myPublished',
+    component: '/myPublished/index',
     meta: {
-      icon: 'application',
-      title: '应用管理'
-    },
-    children: [
-      {
-        path: '/privacy/assess',
-        name: 'privacyAssess',
-        component: '/privacy/assess',
-        meta: {
-          icon: '',
-          title: '隐私测评'
-        }
-      },
-      {
-        path: '/privacy/report',
-        name: 'privacy',
-        component: '/privacy/report',
-        meta: {
-          icon: '',
-          title: '隐私报告'
-        }
-      }
-    ]
+      icon: '',
+      title: '我发布的'
+    }
+  },
+  {
+    path: '/onSale/index',
+    name: 'onSale',
+    component: '/onSale/index',
+    meta: {
+      icon: '',
+      title: '在卖'
+    }
+  },
+  {
+    path: '/notice/index',
+    name: 'notice',
+    component: '/notice/index',
+    meta: {
+      icon: '',
+      title: '通知消息'
+    }
+  },
+  {
+    path: '/chat/index',
+    name: 'chat',
+    component: '/chat/index',
+    meta: {
+      icon: '',
+      title: '聊天消息'
+    }
   }
 ]
